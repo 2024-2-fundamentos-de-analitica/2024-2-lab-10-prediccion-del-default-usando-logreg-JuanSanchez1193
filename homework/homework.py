0d7cd48b44aa07aec4f9cee88bd2438f5ba26a90
Juan Jose Pineda Sanchez <jpinedasa@unal.edu.co>
2025-02-22 12:43:41 -0500

    commit
    commit

--- homework/homework.py
+++ homework/homework.py
@@ -1,3 +1,5 @@
+
+
 # flake8: noqa: E501
 #
 # En este dataset se desea pronosticar el default (pago) del cliente el próximo
@@ -95,3 +97,102 @@
 # {'type': 'cm_matrix', 'dataset': 'train', 'true_0': {"predicted_0": 15562, "predicte_1": 666}, 'true_1': {"predicted_0": 3333, "predicted_1": 1444}}
 # {'type': 'cm_matrix', 'dataset': 'test', 'true_0': {"predicted_0": 15562, "predicte_1": 650}, 'true_1': {"predicted_0": 2490, "predicted_1": 1420}}
 #
+
+import json
+import gzip
+import pickle
+from sklearn.preprocessing import OneHotEncoder, MinMaxScaler
+from sklearn.pipeline import Pipeline
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.compose import ColumnTransformer
+from sklearn.feature_selection import SelectKBest, f_classif
+from sklearn.metrics import (
+    precision_score, balanced_accuracy_score, recall_score, f1_score, confusion_matrix
+)
+from sklearn.model_selection import GridSearchCV
+import os
+def pipeline_funcion(cer_atrib, cantid=10):
+    transformer = ColumnTransformer([
+        ('cat', OneHotEncoder(handle_unknown='ignore'), cer_atrib)],
+        remainder='passthrough'
+    )
+    return Pipeline([
+        ('preprocessor', transformer),
+        ('scaler', MinMaxScaler()),
+        ('feature_selection', SelectKBest(score_func=f_classif, k=cantid)),
+        ('classifier', LogisticRegression(max_iter=500, random_state=42))
+    ])
+
+def data_cambiada(df):
+    df.drop(columns=['ID'], inplace=True, errors='ignore')
+    df = df.rename(columns={'default payment next month': 'default'})
+    df.dropna(inplace=True)
+    df = df.query("EDUCATION != 0 and MARRIAGE != 0")
+    df["EDUCATION"] = df["EDUCATION"].clip(upper=4)
+    return df
+
+def metricas_insert(y_true, y_pred, dataset):
+    return {
+        'type': 'metrics',
+        'dataset': dataset,
+        'precision': precision_score(y_true, y_pred),
+        'balanced_accuracy': balanced_accuracy_score(y_true, y_pred),
+        'recall': recall_score(y_true, y_pred),
+        'f1_score': f1_score(y_true, y_pred)
+    }
+
+def hiperparam(pipeline, x_train, y_train):
+    params = {
+        'feature_selection__k': range(1, 11),
+        'classifier__C': [0.001, 0.01, 0.1, 1, 10, 100],
+        'classifier__penalty': ['l1', 'l2'],
+        'classifier__solver': ['liblinear'],
+        "classifier__max_iter": [100, 200]
+    }
+    return GridSearchCV(
+        pipeline, param_grid=params, cv=10, scoring='balanced_accuracy', n_jobs=-1, refit=True
+    )
+
+
+
+def data_insert_conf(y_true, y_pred, dataset):
+    cm = confusion_matrix(y_true, y_pred)
+    return {
+        'type': 'cm_matrix',
+        'dataset': dataset,
+        'true_0': {"predicted_0": int(cm[0, 0]), "predicted_1": int(cm[0, 1])},
+        'true_1': {"predicted_0": int(cm[1, 0]), "predicted_1": int(cm[1, 1])}
+    }
+
+def guardar_info(filepath, estimator):
+    os.makedirs(os.path.dirname(filepath), exist_ok=True)
+    with gzip.open(filepath, 'wb') as f:
+        pickle.dump(estimator, f)
+
+
+test_data = pd.read_csv("files/input/test_data.csv.zip", compression="zip")
+train_data = pd.read_csv("files/input/train_data.csv.zip", compression="zip")
+test_data, train_data = map(data_cambiada, [test_data, train_data])
+    
+x_train, y_train = train_data.drop(columns=['default']), train_data['default']
+x_test, y_test = test_data.drop(columns=['default']), test_data['default']
+categorical_features = x_train.select_dtypes(include=['object', 'category']).columns.tolist()
+    
+pipeline = pipeline_funcion(categorical_features)
+grid_search = hiperparam(pipeline, x_train, y_train)
+grid_search.fit(x_train, y_train)
+    
+guardar_info("files/models/model.pkl.gz", grid_search)
+    
+metrics = [
+    metricas_insert(y_train, grid_search.predict(x_train), 'train'),
+    metricas_insert(y_test, grid_search.predict(x_test), 'test'),
+    data_insert_conf(y_train, grid_search.predict(x_train), 'train'),
+    data_insert_conf(y_test, grid_search.predict(x_test), 'test')
+]
+    
+os.makedirs("files/output/", exist_ok=True)
+with open("files/output/metrics.json", "w") as f:
+    for metric in metrics:
+        f.write(json.dumps(metric) + "\n")
